6a8e48eee68e77507d845101ce9e7d6d249fb9d1
Alex <alexgrimes.se@gmail.com>
2023-04-21 13:46:12 -0400

    adding cards for filtering

---
+++ src/app/pages/home/components/products-header/products-header.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-products-header',
+  templateUrl: `products-header.component.html`,
+  styles: [
+  ]
+})
+export class ProductsHeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
+
